e63c8523fe0b708c8f291bb8521761997422ea8f
MELSTFOR <mforgiarini.89@gmail.com>
2023-12-12 12:34:02 -0300

    route_cat_sobcat

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const { Router } = require('express');
+require("dotenv").config();
+const { Router } = require("express");
 const router = Router();
 
-const products = require('./products.routes.js');
-const users= require('./users.routes.js');
-const types = require ('./types.routes.js')
+const products = require("./products.routes.js");
+const users = require("./users.routes.js");
+const types = require("./types.routes.js");
 //const subTypes = require ('./subTypes.routes.js')
 
-router.use('/products', products);
-router.use('/users', users);
-router.use('/types', types)
+router.use("/products", products);
+router.use("/users", users);
+router.use("/types", types);
 //router.use('subtypes', subTypes)
 
 module.exports = router;
