94628ec2c225e7de87d0656d365915b18969a024
michqiueli <michiqueli@gmail.com>
2023-12-18 15:47:21 -0300

    correcciones menores

--- src/services/filters.services.js
+++ src/services/filters.services.js
@@ -1,72 +1,72 @@
-const { Type, Subtype, Product, Review } = require("../db/db");
-const { Op } = require("sequelize");
+// const { Type, Subtype, Product, Review } = require("../db/db");
+// const { Op } = require("sequelize");
 
-const FiltersServices = {
-  filterBySubtype: async (subtypeName) => {
-    try {
-      const filteredSubtypes = await Product.findAll({
-        where: {
-          "$Subtype.name$": subtypeName,
-        },
-        include: [
-            {
-              model: Type,
-              attributes: ["name"],
-            },
-            {
-              model: Subtype,
-              attributes: ["name", "metric"],
-            },
-            {
-              model: Review
-            }
-          ],
-      });
+// const FiltersServices = {
+//   filterBySubtype: async (subtypeName) => {
+//     try {
+//       const filteredSubtypes = await Product.findAll({
+//         where: {
+//           "$Subtype.name$": subtypeName,
+//         },
+//         include: [
+//             {
+//               model: Type,
+//               attributes: ["name"],
+//             },
+//             {
+//               model: Subtype,
+//               attributes: ["name", "metric"],
+//             },
+//             {
+//               model: Review
+//             }
+//           ],
+//       });
 
-      return filteredSubtypes.map((product) => ({
-        type: product.Subtype.Type.name,
-        subType: product.Subtype.name,
-        metric: product.Subtype.metric,
-      }));
-    } catch (error) {
-      console.error(error);
-      throw new Error("Error filtering products by subtype name");
-    }
-  },
+//       return filteredSubtypes.map((product) => ({
+//         type: product.Subtype.Type.name,
+//         subType: product.Subtype.name,
+//         metric: product.Subtype.metric,
+//       }));
+//     } catch (error) {
+//       console.error(error);
+//       throw new Error("Error filtering products by subtype name");
+//     }
+//   },
 
-  filterByType: async (typeName) => {
-    try {
-      const filteredTypes = await Product.findAll({
-        where: {
-          "$Type.name$": typeName,
-        },
-        include: [
-            {
-              model: Type,
-              attributes: ["name"],
-            },
-            {
-              model: Subtype,
-              attributes: ["name", "metric"],
-            },
-            {
-              model: Review
-            }
-          ],
-      });
+//   filterByType: async (typeName) => {
+//     try {
+//       const filteredTypes = await Product.findAll({
+//         where: {
+//           "$Type.name$": typeName,
+//         },
+//         include: [
+//             {
+//               model: Type,
+//               attributes: ["name"],
+//             },
+//             {
+//               model: Subtype,
+//               attributes: ["name", "metric"],
+//             },
+//             {
+//               model: Review
+//             }
+//           ],
+//       });
 
-      return filteredTypes.map((product) => ({
-        typeName: product.Type.name,
-        subtypes: product.Subtype.map((subtype) => ({
-          name: subtype.name,
-          metric: subtype.metric,
-        })),
-      }));
-    } catch (error) {
-      console.error(error);
-      throw new Error("Error filtering products by type name");
-    }
-  },
-};
+//       return filteredTypes.map((product) => ({
+//         typeName: product.Type.name,
+//         subtypes: product.Subtype.map((subtype) => ({
+//           name: subtype.name,
+//           metric: subtype.metric,
+//         })),
+//       }));
+//     } catch (error) {
+//       console.error(error);
+//       throw new Error("Error filtering products by type name");
+//     }
+//   },
+// };
 
-module.exports = FiltersServices;
+// module.exports = FiltersServices;
