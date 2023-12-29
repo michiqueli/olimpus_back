6d70524811b0d124b45a783a52c8b0ddbde07636
ProfeCandy <matias.quintanaballesteros@gmail.com>
2023-12-29 17:38:59 -0300

    Login con Google teoricamente andando desde el back

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,10 +7,12 @@ const users = require("./users.routes.js");
 const types = require("./types.routes.js");
 const carts = require("./carts.routes.js");
 const purchases = require("./compras.routes.js");
+const login = require("./login.routes.js")
 //const subTypes = require ('./subTypes.routes.js')
 
 router.use("/products", products);
 router.use("/users", users);
+router.use("/login", login)
 router.use("/types", types);
 router.use("/carts", carts);
 router.use("/purchases", purchases); //! historial de compras x usuario
